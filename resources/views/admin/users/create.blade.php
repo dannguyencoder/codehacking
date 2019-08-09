bfd9ed08ca51d6694b33c8ecf7373315f618b173
Daniel Nguyen <danielnguyen@Daniels-MBP.net.fpt>
2019-08-09 14:36:51 +0700

    form create user

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Create User</h1>
+
+    {!! Form::open(['method' => 'POST', 'action' => 'AdminUsersController@store', 'files'=> true]) !!}
+
+        <div class="form-group">
+
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+
+        </div>
+
+        {!! Form::close() !!}
+
+@stop
