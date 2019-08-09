40a2fb3ab080bed8efcfc764a30ff9abc4bacc4a
Daniel Nguyen <danielnguyen@Daniels-MBP.net.fpt>
2019-08-09 15:44:17 +0700

    users create

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Photo;
+use App\Role;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -32,7 +35,9 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.create');
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -41,9 +46,35 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+
+        $input = $request->all();
+
+        if ($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
+
+
+//        return redirect('/admin/users');
+
+//        return $request->all();
+
     }
 
     /**
